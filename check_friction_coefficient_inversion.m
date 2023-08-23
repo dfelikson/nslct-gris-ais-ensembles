b03ed958f1ab2e80855ea0312991517c58d6118d
Denis Felikson <dfelikso@gs615-searise.ndc.nasa.gov>
2023-08-23 15:09:40 -0400

    Added code to plot histograms of model-obs velocity differences for multiple models

--- check_friction_coefficient_inversion.m
+++ check_friction_coefficient_inversion.m
@@ -1,3 +1,4 @@
+% Plots for one model
 h = figure;
 plotmodel(md, 'data', md.results.StressbalanceSolution.FrictionCoefficient-md.friction.coefficient, 'mask#all', md.mask.ice_levelset<0, 'caxis', [-25 25], 'figure', h.Number)
 
@@ -9,4 +10,19 @@ figure;
 hist(md.results.StressbalanceSolution.Vel(pos) - md.inversion.vel_obs(pos), 1000);
 mean(md.results.StressbalanceSolution.Vel(pos) - md.inversion.vel_obs(pos))
 std(md.results.StressbalanceSolution.Vel(pos) - md.inversion.vel_obs(pos))
+
+% Histograms for all models -- high-velocity points
+pos = md1.mask.ice_levelset<0 & md1.inversion.vel_obs>1000;
+vel_diff_1 = md1.results.StressbalanceSolution.Vel(pos) - md1.inversion.vel_obs(pos);
+vel_diff_2 = md2.results.StressbalanceSolution.Vel(pos) - md2.inversion.vel_obs(pos);
+vel_diff_3 = md3.results.StressbalanceSolution.Vel(pos) - md3.inversion.vel_obs(pos);
+vel_diff_4 = md4.results.StressbalanceSolution.Vel(pos) - md4.inversion.vel_obs(pos);
+
+close all;
+figure; hold on;
+histogram(vel_diff_1, 1000, 'FaceAlpha', 0.5);
+histogram(vel_diff_2, 1000, 'FaceAlpha', 0.5);
+histogram(vel_diff_3, 1000, 'FaceAlpha', 0.5);
+histogram(vel_diff_4, 1000, 'FaceAlpha', 0.5);
+xlim([-250,250])
 
