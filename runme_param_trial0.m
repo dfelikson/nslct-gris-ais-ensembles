cbb80092e4b56bab405dcbe4bdb2570d7fcbed9e
Denis Felikson <dfelikso@gs615-searise.ndc.nasa.gov>
2023-08-23 16:20:32 -0400

    Adding scripts for trial 0, which is the same as what is in the main branch

---
+++ runme_param_trial0.m
@@ -0,0 +1,9 @@
+% load in refined mesh
+md = loadmodel('./models/initial_mesh_setup/gris.mesh.mat');
+
+%md = setmask(md,'',''); % empty arguements denotes that all ice is grounded
+md = parameterize(md,'./Par/Greenland_trial0.par');
+md = setflowequation(md,'SSA','all');
+
+save(['./models/inversion_trials/gris.param.trial0.mat'], 'md');
+
