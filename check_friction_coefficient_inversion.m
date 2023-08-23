9e95769bf134b3489fba052c18f594ab4d15f655
Denis Felikson <dfelikso@gs615-searise.ndc.nasa.gov>
2023-08-23 16:36:58 -0400

    Added md0

--- check_friction_coefficient_inversion.m
+++ check_friction_coefficient_inversion.m
@@ -12,7 +12,8 @@ mean(md.results.StressbalanceSolution.Vel(pos) - md.inversion.vel_obs(pos))
 std(md.results.StressbalanceSolution.Vel(pos) - md.inversion.vel_obs(pos))
 
 % Histograms for all models -- high-velocity points
-pos = md1.mask.ice_levelset<0 & md1.inversion.vel_obs>1000;
+pos = md0.mask.ice_levelset<0 & md0.inversion.vel_obs>1000;
+vel_diff_0 = md0.results.StressbalanceSolution.Vel(pos) - md0.inversion.vel_obs(pos);
 vel_diff_1 = md1.results.StressbalanceSolution.Vel(pos) - md1.inversion.vel_obs(pos);
 vel_diff_2 = md2.results.StressbalanceSolution.Vel(pos) - md2.inversion.vel_obs(pos);
 vel_diff_3 = md3.results.StressbalanceSolution.Vel(pos) - md3.inversion.vel_obs(pos);
@@ -20,6 +21,7 @@ vel_diff_4 = md4.results.StressbalanceSolution.Vel(pos) - md4.inversion.vel_obs(
 
 close all;
 figure; hold on;
+histogram(vel_diff_0, 1000, 'FaceAlpha', 0.5);
 histogram(vel_diff_1, 1000, 'FaceAlpha', 0.5);
 histogram(vel_diff_2, 1000, 'FaceAlpha', 0.5);
 histogram(vel_diff_3, 1000, 'FaceAlpha', 0.5);
